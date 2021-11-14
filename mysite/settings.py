f7f05dec6054e3b864cfb535756019d79f3a031a
kurtknopfler <callmezz@126.com>
2021-11-14 15:19:12 +0800

    add allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i^m3^#^y1loz-rfrke8_!#*w&#boot422_eeycv*4+!e5(ex6k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kurknopfler.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
